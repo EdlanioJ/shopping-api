daaa4c2af88669d467c1f759cde44c6ac4f4eac6
Edlânio Manuel <edlanioj@gmail.com>
2024-02-13 17:30:40 +0100

    feat: add and config drizzle
    Add drizzle lib to handle with database connection
    this first configs is been using neon
    bd because is one of db that offers support
    
    ref:
    - [Drizzle docs](https://orm.drizzle.team/docs/overview)

---
+++ src/db/schema/users.ts
@@ -0,0 +1,13 @@
+import { createId } from '@paralleldrive/cuid2'
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+
+export const users = pgTable('users', {
+  id: text('id')
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  name: text('name').notNull(),
+  email: text('email').notNull().unique(),
+  password: text('password').notNull(),
+  createdAt: timestamp('created_at').defaultNow(),
+  updatedAt: timestamp('updated_at').defaultNow(),
+})
